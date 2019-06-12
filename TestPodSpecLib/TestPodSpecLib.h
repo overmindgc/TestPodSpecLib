107a4505517ac750eaa5f74bdae99d87f8be733a
chen.gong <chen.gong@dmall.com>
2019-06-12 10:41:02 +0800

    Initial Commit

---
+++ TestPodSpecLib/TestPodSpecLib.h
@@ -0,0 +1,13 @@
+//
+//  TestPodSpecLib.h
+//  TestPodSpecLib
+//
+//  Created by GC on 2019/6/12.
+//  Copyright © 2019 Dmall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestPodSpecLib : NSObject
+
+@end
---
+++ TestPodSpecLib/TestPodSpecLib.m
@@ -0,0 +1,13 @@
+//
+//  TestPodSpecLib.m
+//  TestPodSpecLib
+//
+//  Created by GC on 2019/6/12.
+//  Copyright © 2019 Dmall. All rights reserved.
+//
+
+#import "TestPodSpecLib.h"
+
+@implementation TestPodSpecLib
+
+@end
